b13a6405a7fa43e6ca3a01f666697ebdf9782970
Linka <Polina153@mail.ru>
2025-12-12 14:59:26 +0400

    Explode

--- app/src/main/java/com/example/kotlinanimations/MainActivity.kt
+++ app/src/main/java/com/example/kotlinanimations/MainActivity.kt
@@ -1,24 +1,29 @@
 package com.example.kotlinanimations
 
 import android.annotation.SuppressLint
+import android.graphics.Rect
 import android.os.Bundle
-import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.kotlinanimations.databinding.ActivityMainBinding
-import androidx.transition.Slide
+import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Explode
+import androidx.transition.Fade
+import androidx.transition.Transition
+import androidx.transition.TransitionListenerAdapter
 import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
+import com.example.kotlinanimations.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
-    private var textIsVisible = false
-
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,11 +37,71 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.button.setOnClickListener {
-            TransitionManager.beginDelayedTransition(binding.main,  Slide(Gravity.END))
-            textIsVisible = !textIsVisible
-            binding.text.visibility = if (textIsVisible) View.VISIBLE else View.GONE
+        binding.recyclerView.adapter = Adapter()
+    }
+
+    private fun explode(clickedView: View) {
+        val viewRect = Rect()
+        clickedView.getGlobalVisibleRect(viewRect)
+        val explode = Explode()
+        explode.epicenterCallback = object : Transition.EpicenterCallback() {
+            override fun onGetEpicenter(transition: Transition): Rect {
+                return viewRect
+            }
+        }
+        explode.duration = 1000
+        TransitionManager.beginDelayedTransition(binding.recyclerView, explode)
+        binding.recyclerView.adapter = null
+    }
+
+    /*private fun explode(clickedView: View) {
+        val viewRect = Rect()
+        clickedView.getGlobalVisibleRect(viewRect)
+        val explode = Explode()
+        explode.epicenterCallback = object : Transition.EpicenterCallback() {
+            override fun onGetEpicenter(transition: Transition): Rect {
+                return viewRect
+            }
+        }
+        explode.duration = 3000
+        explode.excludeTarget(clickedView, true)
+        val set = TransitionSet()
+            .addTransition(explode)
+            .addTransition(Fade().addTarget(clickedView))
+            .addListener(object : TransitionListenerAdapter() {
+                override fun onTransitionEnd(transition: Transition) {
+                    transition.removeListener(this)
+                    onBackPressed()
+                }
+            })
+        TransitionManager.beginDelayedTransition(binding.recyclerView, set)
+        binding.recyclerView.adapter = null
+    }*/
+
+
+    inner class Adapter : RecyclerView.Adapter<ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.activity_animations_explode_recycle_view_item,
+                    parent,
+                    false
+                ) as View
+            )
         }
 
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.itemView.setOnClickListener {
+                explode(it)
+            }
+        }
+
+
+        override fun getItemCount(): Int {
+            return 32
+        }
     }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
